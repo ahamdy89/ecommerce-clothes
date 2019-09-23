c421332d31767052ffdeb33f4b917cb87caffb3e
ahamdy89 <ahmedhamdy_89@hotmail.com>
2019-09-23 19:19:33 +0200

    adding the collections routes depends on the collection id that retrieve only items for the collection

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import {Route} from 'react-router-dom';
 
-const ShopPage = () =>{
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';
+
+const ShopPage = ({match}) =>{
     return (
         <div className="shop-page">
-            <CollectionsOverview/>
+            <Route exact path= {`${match.path}`} component={CollectionsOverview}/>
+            <Route exact path= {`${match.path}/:collectionId`} component={CollectionPage}/>
         </div>
     );
 }
