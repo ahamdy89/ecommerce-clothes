5ea51db1f7080af7bdc19fc71568fe05199be041
ahamdy89 <ahmedhamdy_89@hotmail.com>
2019-09-23 14:45:07 +0200

    creating add item and remove item functions to increase and decrease quantity till removing it inside the checkout page

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const cartTypes = {
     TOGGLE_CART: 'TOGGLE_CART',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    CLEAR_CART_ITEM: 'CLEAR_CART_ITEM' 
 }
 
 export default cartTypes;
